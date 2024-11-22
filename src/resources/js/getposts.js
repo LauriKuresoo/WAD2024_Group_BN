297c6f0404fd06d06667c58bfb2247fa1daa9daf
Lauri Kuresoo <lauri.kuresoo@ut.ee>
2024-11-22 09:10:21 +0200

    paths fixed

--- src/resources/js/getposts.js
+++ src/resources/js/getposts.js
@@ -2,35 +2,30 @@ POSTS_URI = "https://api.jsonbin.io/v3/qs/672a1ea2e41b4d34e44edcb5"
 AUTHORS_URI = "https://api.jsonbin.io/v3/qs/672a259fe41b4d34e44ee17d"
 
 async function getAuthorImg(name) {
-    const response = await fetch(AUTHORS_URI);   //src/resources/data/authors.json
-    const authors_json = await response.json();
-    const authors = authors_json.record;
+    const response = await fetch("/src/resources/data/authors.json");
+    const authors = await response.json();
     const author = authors.find(author => author.name === name);
     return author ? author.img : "/src/resources/images/karu.jpeg";
 };
-
 function addLike(id){
     const likes = parseInt(document.getElementById(`like-${id}`).textContent) + 1;
     document.getElementById(`like-${id}`).textContent = likes;
 }
 
-
-
-fetch(POSTS_URI)   //src/resources/data/posts.json
+fetch(POSTS_URI)
     .then(response => response.json())
-    .then(async result => {
-        const posts = result.record;
+    .then(data => data.record)
+    .then(async posts => {
         const postList = document.querySelector(".posts");
-        //console.log("posts: " + posts)
+        
         for (const [index, post] of posts.entries()) {
             post.id = index;
-            
+
             const authorImg = await getAuthorImg(post.author);
             const postImage = post.image ? post.image : null;
-
             let div = document.createElement("div");
             div.classList.add("post");
-            
+
             let postHeader = document.createElement("div");
             postHeader.classList.add("post-header");
             postHeader.innerHTML = `
@@ -43,47 +38,31 @@ fetch(POSTS_URI)   //src/resources/data/posts.json
             postContent.innerHTML = `
                     <p>${post.text}</p>
                 `;
+
             if(postImage){
                 let img = document.createElement("img");
                 img.src = postImage;
                 img.classList.add("img-container");
                 postContent.appendChild(img);
             }
-            
+
             let postFooter = document.createElement("div");
             postFooter.classList.add("post-footer");
+        
             postFooter.innerHTML = `
                         <button class="like-button" onclick="addLike(${post.id})">
                             <img src="/src/resources/images/like.png" alt="like">
                         </button>
                         <span class="like-count" id="like-${post.id}">${post.likes}</span>
                         `;
-            
-            
-            
+
+
+
             div.append(postHeader, postContent, postFooter);
             //div.appendChild(postHeader);
-            
+
             postList.appendChild(div);
-          
+
         }
     })
     .catch(error => console.error("Error loading posts:", error));
- 
-          
-
-
-    /*<div class="post" id="1"> 
-        <div class="post-header">
-          <img src="karu.jpeg">
-          <h3>Oct 22, 2024</h3>
-        </div>
-        <div class="post-content">
-          <p>Pls give us max points. We have Robin in the hood. Robin Hood</p>
-        </div>
-        <div class="post-footer">
-            <button>  
-              <img id="like" src="like.png">
-            </button>
-        </div>
-      </div>*/
