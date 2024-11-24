e98ed7149c77fbe67920619bea4effa596b00b26
Lauri Kuresoo <lauri.kuresoo@ut.ee>
2024-11-24 11:27:43 +0200

    release

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/WAD2024_Group_BN/" : "/",
+  publicPath: "/WAD2024_Group_BN/",
 })
