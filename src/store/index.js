e939d0d87331b16a545526d65b21be937f9222e9
Agnes Kivistik <agnes.kivistik@ut.ee>
2024-11-25 23:13:50 +0200

    yl4.2 like on voimalik suurendada nupu vajutusega

--- src/store/index.js
+++ src/store/index.js
@@ -2,27 +2,35 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    postsList:[],
+    postsList: [],
     POSTS_URI: "resources/data/posts.json"
-    
+
   },
   getters: {
-    getPostsList : (state) => state.postsList,
+    getPostsList: (state) => state.postsList,
   },
   mutations: {
-    setPostsList : (state, posts) => state.postsList = posts
-    
+    setPostsList: (state, posts) => state.postsList = posts,
+    IncreaseLike(state, postId) {
+      const post = state.postsList.find(p => p.id === postId);
+      if (post) {
+        post.likes++;
+      }
+    }
+
   },
   actions: {
-    async fetchPosts({commit, state}) {
+    async fetchPosts({ commit, state }) {
       fetch('http://localhost:3000/posts')
-      .then(response => response.json())
-      .then(posts => {
+        .then(response => response.json())
+        .then(posts => {
           console.log(posts);
           commit("setPostsList", posts)
-      })
-      .catch(error => console.error("Error loading posts:", error));
-  
+        })
+        .catch(error => console.error("Error loading posts:", error));
+
+    }, IncreaseLikeAct({ commit }, postId) {
+      commit('IncreaseLike', postId);
     }
   },
   modules: {
