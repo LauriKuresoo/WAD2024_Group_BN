0937adf6bf2eb41259f17551e845a0187dfea173
RobinMurk <116725064+RobinMurk@users.noreply.github.com>
2024-12-16 19:58:37 +0200

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "aXsyO9tv", 
+    password: "sql", 
     database: "WAD_HW4",
     host: "localhost",
     port: "5432"
