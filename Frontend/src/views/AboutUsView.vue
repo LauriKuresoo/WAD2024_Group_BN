24732dc6d2d6bb2b7f35effa58f659e0741bc29e
Agnes Kivistik <agnes.kivistik@ut.ee>
2024-12-17 21:28:50 +0200

    about us sisu

--- Frontend/src/views/AboutUsView.vue
+++ Frontend/src/views/AboutUsView.vue
@@ -1,5 +1,10 @@
 <template>
-    
+    <div class="info">
+        <h3>TEAM BN</h3>
+        <p>
+            Agnes Kivistik, Lauri Kuresoo and Robin Mürk</p>
+            <a href="https://github.com/LauriKuresoo/WAD2024_Group_BN.git">GITHUB</a>
+    </div>
 </template>
 
 <script>
@@ -9,5 +14,13 @@ export default {
 </script>
 
 <style>
-
+.info {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    margin: 20px auto;
+    background-color: bisque;
+    border-radius: 20px;
+    padding: 20px;
+}
 </style>
